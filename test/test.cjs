9578f11df1a13443a123fddebf41b61f27d401b6
Anthony Beaumont <dev.xan105@outlook.be>
2020-12-21 16:09:35 +0700

    pretty

--- test/test.cjs
+++ test/test.cjs
@@ -2,4 +2,7 @@
 
 const capture = require("../lib/capture.cjs");
 
-capture.h264_hwencode("./dump/vid eo.mp4", "nvidia", { overwrite: true }).then(console.log).catch(console.error);
+capture
+  .h264_hwencode("./dump/vid eo.mp4", "nvidia", { overwrite: true })
+  .then(console.log)
+  .catch(console.error);
