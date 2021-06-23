f7bf77a3d41dca8a69ef33511f266c5b2b093efb
Anthony Beaumont <dev.xan105@outlook.be>
2021-06-23 15:46:12 +0700

    sync

--- test/test.cjs
+++ test/test.cjs
@@ -2,10 +2,20 @@
 
 const capture = require("../lib/capture.cjs");
 
-capture
-  .h264_hwencode("./dump/vid eo.mp4", "nvidia", {
+capture.hevc_hwencode("./dump/vid eo hevc.mp4", "nvidia", {
+    framerate: 60,
+    bits10: false,
     overwrite: true,
     audioInterface: "virtual-audio-capturer",
-  })
-  .then(console.log)
-  .catch(console.error);
+})
+.then(console.log)
+.catch(console.error);
+
+/*capture.h264_hwencode("./dump/vid eo h264.mp4", "nvidia", {
+    framerate: 30,
+    bits10: true,
+    overwrite: true,
+    audioInterface: "virtual-audio-capturer",
+})
+.then(console.log)
+.catch(console.error);*/
